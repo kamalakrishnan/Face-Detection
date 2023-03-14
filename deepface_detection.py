7eadcfbbf77b7654dec829d75ae7521a53180aef
Kamala Krishnan <77208924+kamalakrishnan@users.noreply.github.com>
2023-03-14 01:08:24 -0400

    Update deepface_detection.py

--- deepface_detection.py
+++ deepface_detection.py
@@ -1,10 +1,5 @@
-# -*- coding: utf-8 -*-
 from deepface import DeepFace
 import cv2
-
-
-# detector_backend = "opencv", "ssd", "dlib", "mtcnn", "retinaface"
-
 #face detection and face alignment
 face_detected = DeepFace.detectFace(img_path="dataset/testing/modi1.jpg",
                                     detector_backend="opencv")
