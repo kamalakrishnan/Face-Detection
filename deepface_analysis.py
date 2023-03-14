071404a6f2585994e1c55eb74f7e50b7c7e3ce99
Kamala Krishnan <77208924+kamalakrishnan@users.noreply.github.com>
2023-03-14 01:07:44 -0400

    Update deepface_analysis.py

--- deepface_analysis.py
+++ deepface_analysis.py
@@ -1,10 +1,4 @@
-# -*- coding: utf-8 -*-
 from deepface import DeepFace
-
-
-#face analysis
 face_analysis = DeepFace.analyze(img_path="dataset/testing/modi1.jpg",
                                     actions=['emotion','age','gender','race'])
-
 print(face_analysis)
-
