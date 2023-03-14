951669a95f180f7bcb2d573979f0d7e3d23183be
Kamala Krishnan <77208924+kamalakrishnan@users.noreply.github.com>
2023-03-14 01:08:55 -0400

    Update deepface_recognition.py

--- deepface_recognition.py
+++ deepface_recognition.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
 from deepface import DeepFace
-
-
-# detector_backend = "opencv", "ssd", "dlib", "mtcnn", "retinaface"
-# model_name = "VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace"
-# distance_metric = "cosine", "euclidean", "euclidean_l2"
-
-
-#face recognition
 face_recognition = DeepFace.find(img_path="dataset/testing/modi1.jpg",
                                     db_path="dataset/training",
                                     detector_backend="opencv",
