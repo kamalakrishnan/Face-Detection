a1b1cb887d52fb06161a3ad4189c80be0846f005
Kamala Krishnan <77208924+kamalakrishnan@users.noreply.github.com>
2023-03-14 01:13:32 -0400

    Update deepface_verification.py

--- deepface_verification.py
+++ deepface_verification.py
@@ -1,13 +1,4 @@
-# -*- coding: utf-8 -*-
 from deepface import DeepFace
-
-
-# detector_backend = "opencv", "ssd", "dlib", "mtcnn", "retinaface"
-# model_name = "VGG-Face", "Facenet", "Facenet512", "OpenFace", "DeepFace"
-# distance_metric = "cosine", "euclidean", "euclidean_l2"
-
-
-#face verification
 face_verified = DeepFace.verify(img1_path="dataset/testing/modi1.jpg",
                                     img2_path="dataset/testing/joe1.jpg",
                                     detector_backend="opencv",
