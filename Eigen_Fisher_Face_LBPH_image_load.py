c56c6fc53cae8499b7d9ce7f40fdf88c9ba0c3f6
Kamala Krishnan <77208924+kamalakrishnan@users.noreply.github.com>
2023-03-14 01:06:18 -0400

    Update Eigen_Fisher_Face_LBPH_image_load.py

--- Eigen_Fisher_Face_LBPH_image_load.py
+++ Eigen_Fisher_Face_LBPH_image_load.py
@@ -1,14 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-
-@author: abhi
-"""
-
-# install opencv-contrib using the command
-# pip install --user opencv-contrib-python
-
-
-#import the required libraries
 import cv2
 from sys import exit
 
